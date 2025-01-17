dc1d5ff22b18dddcdd7d2823e126f8e6115968ce
machadof90 <valterig@hotmail.com>
2025-01-17 00:01:14 -0300

    adicionando  .build no gitignore

--- include/cores.c
+++ include/cores.c
@@ -5,76 +5,92 @@
 // Funções para controle de cores
 
 // Função para apagar todos os leds
-void apagado(int tempo){
-gpio_put(LED_R, 0); 
-gpio_put(LED_B, 0); 
-gpio_put(LED_G, 0);
-if(tempo > 0){ // Se o tempo for maior que 0, o programa espera o tempo definido
-sleep_ms(tempo);
-}
+void apagado(int tempo)
+{
+  gpio_put(LED_R, 0);
+  gpio_put(LED_B, 0);
+  gpio_put(LED_G, 0);
+  if (tempo > 0)
+  { // Se o tempo for maior que 0, o programa espera o tempo definido
+    sleep_ms(tempo);
+  }
 }
 // Função para acender o led B e G
-void ciano(int tempo){
-gpio_put(LED_R, 0);  
-gpio_put(LED_B, 1); 
-gpio_put(LED_G, 1);
-if(tempo > 0){  // Se o tempo for maior que 0, o programa espera o tempo definido
-sleep_ms(tempo);
-}
+void ciano(int tempo)
+{
+  gpio_put(LED_R, 0);
+  gpio_put(LED_B, 1);
+  gpio_put(LED_G, 1);
+  if (tempo > 0)
+  { // Se o tempo for maior que 0, o programa espera o tempo definido
+    sleep_ms(tempo);
+  }
 }
 // Função para acender o led R, G e B
-void branco(int tempo){
-gpio_put(LED_R, 1);  
-gpio_put(LED_B, 1); 
-gpio_put(LED_G, 1);
-if(tempo > 0){ // Se o tempo for maior que 0, o programa espera o tempo definido
-sleep_ms(tempo);
-}
+void branco(int tempo)
+{
+  gpio_put(LED_R, 1);
+  gpio_put(LED_B, 1);
+  gpio_put(LED_G, 1);
+  if (tempo > 0)
+  { // Se o tempo for maior que 0, o programa espera o tempo definido
+    sleep_ms(tempo);
+  }
 }
 // Função para acender o led R e B
-void rosa(int tempo){
-gpio_put(LED_R, 1); 
-gpio_put(LED_B, 1);  
-gpio_put(LED_G, 0);
-if(tempo > 0){// Se o tempo for maior que 0, o programa espera o tempo definido
-sleep_ms(tempo);
-}
+void rosa(int tempo)
+{
+  gpio_put(LED_R, 1);
+  gpio_put(LED_B, 1);
+  gpio_put(LED_G, 0);
+  if (tempo > 0)
+  { // Se o tempo for maior que 0, o programa espera o tempo definido
+    sleep_ms(tempo);
+  }
 }
 // Função para acender o led R e G
-void amarelo(int tempo){
-gpio_put(LED_R, 1); 
-gpio_put(LED_B, 0);  
-gpio_put(LED_G, 1);
-if(tempo > 0){// Se o tempo for maior que 0, o programa espera o tempo definido
-sleep_ms(tempo);
-}
+void amarelo(int tempo)
+{
+  gpio_put(LED_R, 1);
+  gpio_put(LED_B, 0);
+  gpio_put(LED_G, 1);
+  if (tempo > 0)
+  { // Se o tempo for maior que 0, o programa espera o tempo definido
+    sleep_ms(tempo);
+  }
 }
 // Função para acender o led R
-void vermelho(int tempo){
-gpio_put(LED_R, 1); 
-gpio_put(LED_B, 0);  
-gpio_put(LED_G, 0);
-if(tempo > 0){ // Se o tempo for maior que 0, o programa espera o tempo definido
-sleep_ms(tempo);
-}
+void vermelho(int tempo)
+{
+  gpio_put(LED_R, 1);
+  gpio_put(LED_B, 0);
+  gpio_put(LED_G, 0);
+  if (tempo > 0)
+  { // Se o tempo for maior que 0, o programa espera o tempo definido
+    sleep_ms(tempo);
+  }
 }
 // Função para acender o led B
-void azul(int tempo){
-gpio_put(LED_R, 0); 
-gpio_put(LED_B, 1); 
-gpio_put(LED_G, 0);
-if(tempo > 0){ // Se o tempo for maior que 0, o programa espera o tempo definido
-sleep_ms(tempo);
-}
+void azul(int tempo)
+{
+  gpio_put(LED_R, 0);
+  gpio_put(LED_B, 1);
+  gpio_put(LED_G, 0);
+  if (tempo > 0)
+  { // Se o tempo for maior que 0, o programa espera o tempo definido
+    sleep_ms(tempo);
+  }
 }
 // Função para acender o led G
-void verde(int tempo){
-gpio_put(LED_R, 0); 
-gpio_put(LED_B, 0); 
-gpio_put(LED_G, 1); 
-if(tempo > 0){// Se o tempo for maior que 0, o programa espera o tempo definido
-sleep_ms(tempo);
-}
+void verde(int tempo)
+{
+  gpio_put(LED_R, 0);
+  gpio_put(LED_B, 0);
+  gpio_put(LED_G, 1);
+  if (tempo > 0)
+  { // Se o tempo for maior que 0, o programa espera o tempo definido
+    sleep_ms(tempo);
+  }
 }
 
-//fim do arquivo cores.c
+// fim do arquivo cores.c
--- include/iniciarPinos.c
+++ include/iniciarPinos.c
@@ -16,21 +16,21 @@
 #define MATRIX_R3 6
 #define MATRIX_R2 7
 
-//inicia os componentes
-void iniciaComponentes(){
-//iniciando LEDs
-gpio_init(LED_B);
-gpio_set_dir(LED_B, GPIO_OUT);
-gpio_init(LED_R);
-gpio_set_dir(LED_R, GPIO_OUT);
-gpio_init(LED_G);
-gpio_set_dir(LED_G, GPIO_OUT);
-//inciando buzzers
-gpio_init(BUZZER_A);
-gpio_set_dir(BUZZER_A, GPIO_OUT);
-gpio_init(BUZZER_B);
-gpio_set_dir(BUZZER_B, GPIO_OUT);
-
+// inicia os componentes
+void iniciaComponentes()
+{
+  // iniciando LEDs
+  gpio_init(LED_B);
+  gpio_set_dir(LED_B, GPIO_OUT);
+  gpio_init(LED_R);
+  gpio_set_dir(LED_R, GPIO_OUT);
+  gpio_init(LED_G);
+  gpio_set_dir(LED_G, GPIO_OUT);
+  // inciando buzzers
+  gpio_init(BUZZER_A);
+  gpio_set_dir(BUZZER_A, GPIO_OUT);
+  gpio_init(BUZZER_B);
+  gpio_set_dir(BUZZER_B, GPIO_OUT);
 }
 
-//fim do arquivo
+// fim do arquivo
