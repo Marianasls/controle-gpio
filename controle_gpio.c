dd2bb9d9d3d268aeaa564912b37f768376707e1b
machadof90 <valterig@hotmail.com>
2025-01-17 00:25:40 -0300

    adiciona implementação do KeyPad em KeyPad.c e remove arquivo antigo

--- controle_gpio.c
+++ controle_gpio.c
@@ -19,7 +19,10 @@
 #define MATRIX_R3 6
 #define MATRIX_R2 7
 #define MATRIX_R1 8
-
+#define ROWS 4
+#define COLS 4
+const uint8_t Row_Pins[ROWS] = {8, 7, 6, 5}; 
+const uint8_t Col_Pins[COLS] = {1, 2, 3, 4};
 
 int main()
 {
